cc76c2414c623ab92a092af48481482e268f8c5b
Philip Balbas <philip.balbas@gmail.com>
2017-07-29 05:36:30 +0800

    inital commit

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const path = require('path')
+
+const publicPath = path.join(__dirname, '../public')
+const port = process.env.PORT || 3000
+
+const app = express()
+app.use(express.static(publicPath))
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.listen(port, () => {
+  console.log(`Server ready at ${port}`)
+})
